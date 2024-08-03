441ec1f93e1661b6b1723dae6fa4ee0949a42649
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-03 17:20:00 +0300

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
@@ -5,6 +6,10 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center bg-bg-100" id="root">
       <Navbar></Navbar>
       <div className="container h-24"></div>
+      <div>
+      <h1>Welcome to Task-Journal</h1>
+      <Link href="/tasks">View Tasks</Link>
+    </div>
     </main>
   );
 }
