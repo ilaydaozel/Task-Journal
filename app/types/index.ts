b127ea345e3a1b3f019927db1565c520e72f40c1
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-03 17:19:26 +0300

    Create ITask interface

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,17 +1,15 @@
-
-interface IProgram {
-  id?: string;
-  ranking: number | null;
-  name: string;
-  universityName: string | null;
-  programLink: string | null; 
-  courseContent: string | null;
-  requirements: string | null;
-  language: string | null;
-  applicationDeadline: string | null;
-  additionalNotes: string | null;
-  applicationStatus: string | null;
+interface ITask {
+  id?: string;                    // Unique identifier for the task
+  name: string;                  // Name of the task
+  description: string | null;           // Detailed description of the task
+  acceptanceCriteria: string | null;    // Criteria for accepting the task as completed
+  deadline: Date | null;                // Deadline for the task
+  status: 'to-do' | 'in progress' | 'done'; // Current status of the task
+  comments: string[];            // List of comments associated with the task
+  createdAt: Date;               // Timestamp when the task was created
+  updatedAt: Date;               // Timestamp when the task was last updated
 }
+
 
 interface INavLink {
   title: string;
