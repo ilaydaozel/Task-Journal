73bcbe5fee2579eeec2b54552a5a3a641bcc59f4
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-03 17:52:38 +0300

    Create actions

---
+++ app/actions/getTasks.tsx
@@ -0,0 +1,15 @@
+import prisma from "@/app/lib/prismadb";
+
+export default async function getAllTasks(): Promise<ITask[]> {
+  try {
+    const tasksData = await prisma.task.findMany();
+    // Ensure tasksData is defined and is an array
+    if (!tasksData || !Array.isArray(tasksData)) {
+      return []; // Return an empty array if no tasks found
+    }
+    return tasksData;
+  } catch (error: any) {
+    console.error('Error fetching tasks:', error);
+    throw new Error(error);
+  }
+}
