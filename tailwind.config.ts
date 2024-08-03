0f86e5f88060e8b395584f120b4259b021894294
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-03 18:29:15 +0300

    Add custom tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
 
 const config: Config = {
   content: [
@@ -13,6 +14,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      ...colors,
+      primary: colors.green,
+      secondary: colors.sky,
+      bg: colors.slate,
+      text1: colors.gray,
     },
   },
   plugins: [],
