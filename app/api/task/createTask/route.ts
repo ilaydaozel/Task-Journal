190bfba2c584aa120817907edb49792fdd643a23
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-03 17:54:36 +0300

    Create createTask api

---
+++ app/api/task/createTask/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/lib/prismadb";
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    const {
+        name,
+        description,
+        acceptanceCriteria,
+        deadline,
+        status,
+        comments,
+    } = body;
+
+    try {
+        const task: ITask = await prisma.task.create({
+            data: {
+                name: name,
+                description: description,
+                acceptanceCriteria: acceptanceCriteria,
+                deadline: new Date(deadline),
+                status: status,
+                comments: comments,
+            },
+        });
+        return NextResponse.json(task);
+
+    }catch (error) {
+        console.error("Error adding the task:", error);
+        return NextResponse.json({ error: "An error occurred while adding the task: " + error });
+    }
+
+
+    
+}
