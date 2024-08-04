2b0539289d549865613bf1ec42c319fa6f5b3e3c
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-04 13:08:13 +0300

    Change buttons place

--- app/(pages)/taskList/TaskListPage.tsx
+++ app/(pages)/taskList/TaskListPage.tsx
@@ -2,20 +2,10 @@
 
 import React, { useState } from 'react';
 import TaskList from '../../components/lists/TaskList';
-import Button from '@/app/components/formComponents/Button';
-import AddTaskForm from '@/app/components/forms/AddTaskForm';
-
 
 const TaskListPage = ({tasks}:{tasks: ITask[]}) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
   return (
     <div id="taskListPage" className='flex flex-col items-end gap-2 w-[95vw]'>
-      <Button onClick={openModal} label='Add Program'></Button>
-      <AddTaskForm isOpen={isModalOpen} onClose={closeModal} />
       <TaskList tasks={tasks} />
     </div>
   );
--- app/components/lists/TaskList.tsx
+++ app/components/lists/TaskList.tsx
@@ -1,8 +1,19 @@
 import TaskCard from '../taskCard/TaskCard';
+import Button from '@/app/components/formComponents/Button';
+import AddTaskForm from '@/app/components/forms/AddTaskForm';
+import { useState } from 'react';
 
 const TasksPage = ({ tasks = [] }: { tasks?: ITask[] }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-8 flex flex-col gap-8">
+      <div className='text-right'>
+        <Button onClick={openModal} label='Add Program'></Button>      
+        <AddTaskForm isOpen={isModalOpen} onClose={closeModal} />
+      </div>
       <h1 className="text-2xl font-bold mb-4">Your Tasks</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {tasks.length > 0 ? (
