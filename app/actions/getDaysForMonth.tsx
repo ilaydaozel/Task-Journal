df22731464725fb8abc7ca17c6982e2196174cdf
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-05 02:22:31 +0300

    Create getDaysForMonth.tsx

---
+++ app/actions/getDaysForMonth.tsx
@@ -0,0 +1,20 @@
+import prisma from "@/app/lib/prismadb";
+
+
+export default async function getDaysForMonth({ year, month }: { year: number; month: number }): Promise<IDay[] | null> {
+    try {
+        const days = await prisma.day.findMany({
+            where: {
+                date: {
+                    gte: new Date(year, month - 1, 1), // Start of the month
+                    lt: new Date(year, month, 1), // Start of the next month
+                },
+            },
+        });
+
+    return days as unknown as IDay[]; // Ensure the return type matches IDay
+        
+    } catch (error: any) {
+        throw new Error(error.message || error);
+    }
+}
