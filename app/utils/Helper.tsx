6c0012af4d803be46248a8cb0c1928972acd2706
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-04 22:44:47 +0300

    Update Helper.tsx

--- app/utils/Helper.tsx
+++ app/utils/Helper.tsx
@@ -39,3 +39,21 @@ export const handleApiResponse = async (
     }
   }
 };
+
+export const getWeekNumber = (d: Date): [number, number] => {
+  // Copy date so we don't modify the original
+  let date = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  
+  // Set to nearest Thursday: current date + 4 - current day number
+  // Make Sunday's day number 7
+  date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
+  
+  // Get the first day of the year
+  const yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+  
+  // Calculate full weeks to the nearest Thursday
+  const weekNo = Math.ceil((((date.getTime() - yearStart.getTime()) / 86400000) + 1) / 7);
+  
+  // Return an array of year and week number
+  return [date.getUTCFullYear(), weekNo];
+};
