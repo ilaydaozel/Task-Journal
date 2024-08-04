3cc07d164a3ecad71e1d950906540d3e1c984f66
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-05 02:21:59 +0300

    Add new helper functions

--- app/utils/Helper.tsx
+++ app/utils/Helper.tsx
@@ -39,3 +39,38 @@ export const handleApiResponse = async (
     }
   }
 };
+
+export const getWeekNumber = (d: Date): [number, number] => {
+  // Copy date so we don't modify the original
+  let date = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  
+  // Set to nearest Thursday: current date + 4 - current day number
+  // Make Sunday's day number 7
+  date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
+  
+  // Get the first day of the year
+  const yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+  
+  // Calculate full weeks to the nearest Thursday
+  const weekNo = Math.ceil((((date.getTime() - yearStart.getTime()) / 86400000) + 1) / 7);
+  
+  // Return an array of year and week number
+  return [date.getUTCFullYear(), weekNo];
+};
+
+  // Function to get the month name based on the month index (0-11)
+export const getMonthName = (month: number) => {
+    const monthNames = [
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    return monthNames[month] || 'Invalid Month'; // Return the month name or default
+  };
+
+  // Function to get the day of the week based on the day index (0-6)
+export  const getDayOfTheWeek = (dayNumber: number) => {
+    const dayNames = [
+      'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+    ];
+    return dayNames[dayNumber] || 'Invalid Day'; // Return the day name or default
+  };
