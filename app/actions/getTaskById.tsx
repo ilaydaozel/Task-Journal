4f9ec35d694ec3e07145e92fd61e48e85aa58e1c
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-05 02:23:02 +0300

    Rename getTasksById

---
+++ app/actions/getTaskById.tsx
@@ -0,0 +1,28 @@
+import prisma from "@/app/lib/prismadb";
+
+interface IParams {
+    taskId?: string;
+}
+
+export default async function getTaskById(params: IParams): Promise<ITask | null> {
+    try {
+        const { taskId } = params;
+
+        if (!taskId || taskId.length !== 24) {
+            throw new Error("Invalid taskId provided.");
+        }
+
+        const task = await prisma.task.findUnique({
+            where: {
+                id: taskId,
+            },
+            include: {
+                workedOnDays: true, // Include days associated with the task
+            },
+        });
+
+        return task as unknown as ITask;
+    } catch (error: any) {
+        throw new Error(error.message || error);
+    }
+}
