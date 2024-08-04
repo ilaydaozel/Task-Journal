6994b0ebf254fa653c502671f04b8a302bb74140
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-04 14:36:39 +0300

    Create getTaskById action

---
+++ app/actions/getTaskById.ts
@@ -0,0 +1,24 @@
+import prisma from "@/app/lib/prismadb";
+
+interface IParams {
+    taskId?: string;
+}
+
+export default async function getTaskById(
+    params: IParams
+) {
+    try {
+        const { taskId } = params;
+        const task = await prisma.task.findUnique({
+            where: {
+                id: taskId,
+            },
+        });
+        if (!task) {
+            return null;
+        }
+        return task as ITask
+    } catch (error: any) {
+        throw new Error(error);
+    }
+}
