6819a936ea65a700d820f53811deae8a123cee8d
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-05 22:29:18 +0300

    Create DescriptionSection.tsx

---
+++ app/components/task/DescriptionSection.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+const DescriptionSection = ({ task }: { task: ITask }) => {
+  return (
+    <section className="md:w-3/5 w-full mx-auto p-6 bg-white rounded-lg shadow-lg">
+      <h1 className="text-3xl font-bold text-text1-600 mb-4">{task?.name}</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div>
+          <div className="mb-4">
+            <div className="mt-2 text-gray-700">
+              <strong>Description:</strong> {task?.description || "No description available."}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+
+export default DescriptionSection;
