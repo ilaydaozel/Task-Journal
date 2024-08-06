f2897fb94f7b3c1626f3561712411011032ab0d8
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-07 02:34:05 +0300

    Change status colors

--- app/components/views/day/DailyTaskCard.tsx
+++ app/components/views/day/DailyTaskCard.tsx
@@ -8,7 +8,7 @@ const DailyTaskCard = ({ task }: { task: ITask }) => {
     <Link href={`/task/${task.id}`}>
       <div className={`w-full p-6 rounded-md shadow-md mb-2 ${getStatusClassName(task.status)}`}>
         <h3 className="font-semibold">{task.name}</h3>
-        <h3 className="font-semibold">{task.description}</h3>
+        <h3 className="text-sm">{task.description}</h3>
       </div>
     </Link>
   );
