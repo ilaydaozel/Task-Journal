75c180d1a4eb4c3ca916fe3853c6ba84046d21c2
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-10 23:56:33 +0300

    Update week view

--- app/components/views/week/WeekView.tsx
+++ app/components/views/week/WeekView.tsx
@@ -4,7 +4,6 @@ import { printStringDateDM } from '@/app/utils/helper';
 import WeekDayCard from './WeekDayCard';
 
 const WeekView = ({ week}: { week: IWeek | null }) => {
-  console.log("week:", week)
   return (
     <div id="taskListComponent" className="w-full min-h-screen flex flex-col gap-4 items-center mx-auto p-10 md:px-24 md:py-10 bg-primary">
       <h1 className='md:text-2xl text-lg font-bold text-text1-700 px-4 py-2 font-serif'>{printStringDateDM(week?.days[0].date) + " - " + printStringDateDM(week?.days[week.days.length-1].date)}</h1>
@@ -14,7 +13,7 @@ const WeekView = ({ week}: { week: IWeek | null }) => {
             <WeekDayCard key={day.id} day={day} />
           ))
         ) : (
-          <p>No tasks found.</p> // Display a message if there are no tasks
+          <p>No tasks found</p> // Display a message if there are no tasks
         )}
       </div>
     </div>
