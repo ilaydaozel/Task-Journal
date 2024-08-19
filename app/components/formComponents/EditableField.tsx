f252ea87cbaa5df7cebcf02b9cfdc9fbe4dd5b2a
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-08-19 11:18:11 +0300

    Add new colors to the text editor

--- app/components/formComponents/EditableField.tsx
+++ app/components/formComponents/EditableField.tsx
@@ -7,7 +7,6 @@ import { colorStyleMap, fontSizeStyleMap } from './NoteEditor/style';
 
 const convertRawToHTML = (rawContent: string | null): string => {
   if (!rawContent) return '';
-  console.log(rawContent);
   try {
     // Parse the raw JSON
     const contentState = convertFromRaw(JSON.parse(rawContent));
--- app/components/formComponents/NoteEditor/style.ts
+++ app/components/formComponents/NoteEditor/style.ts
@@ -13,13 +13,18 @@ export interface FontSizeMap {
 }
   
 export const colorStyleMap: ColorStyleMap = {
-    red: { color: 'rgba(255, 0, 0, 1.0)' },
+    red: { color: 'rgba(201, 60, 50, 1.0)' },
     orange: { color: 'rgba(255, 127, 0, 1.0)' },
-    yellow: { color: 'rgba(180, 180, 0, 1.0)' },
-    green: { color: 'rgba(0, 180, 0, 1.0)' },
+    yellow: {  color: 'rgba(246, 189, 96, 1.0)' },
     blue: { color: 'rgba(0, 0, 255, 1.0)' },
-    indigo: { color: 'rgba(75, 0, 130, 1.0)' },
+    darkBlue: { color: 'rgba(59, 95, 153, 1.0)' },
+    pink : { color: 'rgba(184, 154, 152, 1.0)' },
     violet: { color: 'rgba(127, 0, 255, 1.0)' },
+    green: { color: 'rgba(111, 184, 138, 1.0)'},
+    darkGreen: { color: 'rgba(89, 130, 119, 1.0)' },
+    gray: { color: 'rgba(107, 114, 128, 1.0)' },
+    darkGray: { color: 'rgba(31, 41, 55, 1.0)' },
+    black: { color: 'rgba(0, 0, 0, 1.0)' },
   };
   
 export const fontSizeStyleMap: FontSizeMap = {
